63fef0f196f2e4cbfc848278fa4ce0f11566b973
mhcrnl <mhcrnl@localhost.localdomain>
2020-09-05 19:59:23 +0300

    Sat 05 Sep 2020 07:59:22 PM EEST

--- 01gitpush.sh
+++ 01gitpush.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-
+#########################################
+# Copyright 2020, by mhcrnl@gmail.com
+#
+#
+# NAME
+#	01gitpush.sh - push code to github
+#
+# SYNOPSIS
+#	./gitpush.sh
+# 
+# DESCRIPTION
+#
+# RETURN VALUE
+#
+###############################################
 DATE=`date`
 
 git add .
